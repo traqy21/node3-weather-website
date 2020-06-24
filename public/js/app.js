26bcd7954a2d09f04d671ab4519da6dc70139364
Aries <traqy21@gmail.com>
2020-06-24 22:38:38 +0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log('Javascript file loaded') 
+ 
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-id-1')
+const message2 = document.querySelector('#message-id-2')
+ 
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
+    const location = search.value
+     
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            
+            if(data.error){
+                message1.textContent = data.error 
+                return
+            }
+ 
+            message1.textContent = data.location
+            message2.textContent = data.forecast
+            
+        })
+    })
+})
